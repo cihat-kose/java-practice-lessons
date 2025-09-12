e96b91a6c3374a41dbbbc37a4a068ab5789cc250
Cihat Köse <cihat.kose@hotmail.com>
2025-09-12 19:19:12 +0200

    Add a new access modifiers and non access modifiers example

---
+++ src/_26_AccessModifiersAndNonAccessModifiers/_07_Config/Config.java
@@ -0,0 +1,5 @@
+package _26_AccessModifiersAndNonAccessModifiers._07_Config;
+
+public class Config {
+    public static final String VERSION = "1.0.0"; // Sabit ve değiştirilemez sürüm bilgisi
+}
---
+++ src/_26_AccessModifiersAndNonAccessModifiers/_07_Config/ConfigApp.java
@@ -0,0 +1,8 @@
+package _26_AccessModifiersAndNonAccessModifiers._07_Config;
+
+public class ConfigApp {
+    public static void main(String[] args) {
+        // Static final değişken: Sınıf adı üzerinden erişim
+        System.out.println("Config Version: " + Config.VERSION);
+    }
+}

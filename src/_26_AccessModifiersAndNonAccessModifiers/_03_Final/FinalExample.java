66b405b9929f424314ebdb0375e53d585722bbc3
Cihat Köse <cihat.kose@hotmail.com>
2025-09-12 19:18:25 +0200

    Add a new access modifiers and non access modifiers example

---
+++ src/_26_AccessModifiersAndNonAccessModifiers/_03_Final/FinalExample.java
@@ -0,0 +1,9 @@
+package _26_AccessModifiersAndNonAccessModifiers._03_Final;
+
+public final class FinalExample {
+    public final int finalVar = 100; // Final değişken: Değiştirilemez
+
+    public final void finalMethod() { // Final metot: Override edilemez
+        System.out.println("This is a final method.");
+    }
+}

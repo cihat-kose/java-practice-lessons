3272b9463d4688128b43982234fabfdaaa3aa2ec
Cihat Köse <cihat.kose@hotmail.com>
2025-09-12 19:18:33 +0200

    Add a new access modifiers and non access modifiers example

---
+++ src/_26_AccessModifiersAndNonAccessModifiers/_04_StaticFinal/StaticFinalValue.java
@@ -0,0 +1,5 @@
+package _26_AccessModifiersAndNonAccessModifiers._04_StaticFinal;
+
+public class StaticFinalValue {
+    public static final int MAX_VALUE = 500; // Static final değişken: Sabit ve değiştirilemez
+}
---
+++ src/_26_AccessModifiersAndNonAccessModifiers/_04_StaticFinal/StaticFinalValueApp.java
@@ -0,0 +1,8 @@
+package _26_AccessModifiersAndNonAccessModifiers._04_StaticFinal;
+
+public class StaticFinalValueApp {
+    public static void main(String[] args) {
+        // Static final değişken: Sınıf adı üzerinden erişim
+        System.out.println("Max Value: " + StaticFinalValue.MAX_VALUE);
+    }
+}

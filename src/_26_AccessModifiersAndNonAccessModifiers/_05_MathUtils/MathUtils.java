70c9781e0d2ec4a8f70778bb71e59ba24db9a491
Cihat Köse <cihat.kose@hotmail.com>
2025-09-12 19:18:42 +0200

    Add a new access modifiers and non access modifiers example

---
+++ src/_26_AccessModifiersAndNonAccessModifiers/_05_MathUtils/MathUtils.java
@@ -0,0 +1,7 @@
+package _26_AccessModifiersAndNonAccessModifiers._05_MathUtils;
+
+public class MathUtils {
+    public static int add(int a, int b) { // Static metot: İki sayıyı toplar
+        return a + b;
+    }
+}

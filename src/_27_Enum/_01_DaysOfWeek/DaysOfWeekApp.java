5ded79a6180ca68a90e161fdd3b8ba73eae147af
Cihat Köse <cihat.kose@hotmail.com>
2025-09-13 18:57:02 +0200

    Add a new enum example

---
+++ src/_27_Enum/_01_DaysOfWeek/DaysOfWeekApp.java
@@ -0,0 +1,8 @@
+package _27_Enum._01_DaysOfWeek;
+
+public class DaysOfWeekApp {
+    public static void main(String[] args) {
+        DaysOfWeek today = DaysOfWeek.CUMA; // Enum sabiti kullanımı
+        System.out.println("Bugün günlerden: " + today);
+    }
+}

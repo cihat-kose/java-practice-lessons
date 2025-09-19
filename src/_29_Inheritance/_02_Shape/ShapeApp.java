c26ad9f8756323b48a5ec6d13555a8df5dcb0c73
Cihat Köse <cihat.kose@hotmail.com>
2025-09-19 23:43:47 +0200

    Add a new example of inheritance

---
+++ src/_29_Inheritance/_02_Shape/ShapeApp.java
@@ -0,0 +1,17 @@
+package _29_Inheritance._02_Shape;
+
+public class ShapeApp {
+    public static void main(String[] args) {
+        // Circle nesnesi oluşturuluyor
+        Circle circle = new Circle(5.0);
+
+        // Rectangle nesnesi oluşturuluyor
+        Rectangle rectangle = new Rectangle(4.0, 6.0);
+
+        // Circle nesnesinin alanı hesaplanıyor
+        circle.calculateArea();  // Circle Area: 78.53981633974483
+
+        // Rectangle nesnesinin alanı hesaplanıyor
+        rectangle.calculateArea();  // Rectangle Area: 24.0
+    }
+}
